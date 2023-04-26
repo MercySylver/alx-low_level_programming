1a917f34cc49a65af338952c7ce7add966e5caf3
MercySylver <mercyben405@gmail.com>
2023-04-26 09:46:10 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d"' n);
+				printf("%d", n);
 				printf("\n");
 				break;
 			}
@@ -23,17 +23,20 @@ void print_to_98(int n)
 			}
 		}
 	}
-	for (; n >= 98; n--)
+	else
 	{
-		if (n == 98)
+		for (; n >= 98; n--)
 		{
-			printf("%d", n);
-			printf("\n");
-			break;
-		}
-		else
-		{
-			printf("%d", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d", n);
+			}
 		}
 	}
 }
